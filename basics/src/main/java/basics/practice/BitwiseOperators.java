48afc5d7056ca0606478f99ac0fc5cb23973fe15
arpit <arpit@Arpiths>
2024-06-24 19:35:34 +0530

    changed the variable

--- basics/src/main/java/basics/practice/BitwiseOperators.java
+++ basics/src/main/java/basics/practice/BitwiseOperators.java
@@ -4,7 +4,7 @@ public class BitwiseOperators {
 
 	public static void main(String[] args) {
 		System.out.println("***bitwise operators***");
-		System.out.println(5 & 6);   
+		System.out.println(7 & 6);   
 		System.out.println(5 | 6);
 		System.out.println(5 ^ 6);
 		System.out.println(~5);
